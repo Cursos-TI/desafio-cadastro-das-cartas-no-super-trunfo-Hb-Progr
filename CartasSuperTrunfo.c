0db651d2f74481f3110cd023dd3af88a7c2793ac
Hb-Progr <hiago_mt@live.com>
2025-07-28 17:50:32 +0000

    CARTA1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,13 +21,44 @@ int main() {
   printf("Desafio Super Trunfo\n");
   printf("Novo commit\n");
 
+  char estado1;
+  char codigo1[10];
+  char nome1[30];
+  int populacao1;
+  float area1;
+  float pib1;
+  int ponto1;
 
+  printf("Digite o estado: ");
+  scanf("%c", &estado1);
 
+  printf("Digite o Código da carta: ");
+  scanf("%s", codigo1); 
 
+  printf("Digite o Nome da Cidade: ");
+  scanf("%s", nome1);
 
+  printf("Digite a População: ");
+  scanf("%d", &populacao1);
 
+  printf("Digite a Área: ");
+  scanf("%f", &area1);
 
+  printf("Digite o PIB: ");
+  scanf("%f", &pib1);
 
+  printf("Digite o Número de pontos turísticos");
+  scanf("%d", &ponto1);
+
+  printf("===Dados da Carta 1 Cadastrados===\n");
+  printf("\n");
+  printf("Estado: %c\n", estado1);
+  printf("Código: %s\n", codigo1);
+  printf("Cidade: %s\n", nome1);
+  printf("População: %d\n", populacao1);
+  printf("Área: %.2f km2 \n,", area1);
+  printf("PIB: %.2f\n", pib1);
+  printf("Pontos: %d\n", ponto1);
 
 }
 
